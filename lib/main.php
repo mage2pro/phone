6d3750c8a3488eae8b2933c6fa094d9bd2e03178
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-01 11:48:19 +0600

    no message

--- lib/main.php
+++ lib/main.php
@@ -69,6 +69,7 @@ function df_phone_format($n, $throw = true, $f) {return df_phone_p()->format(df_
  * 2017-10-22
  * @see df.s.normalizePhone():
  * https://github.com/mage2pro/core/blob/3.2.15/Core/view/base/web/my/string.js#L64-L72 
+ * @used-by \Dfe\Sift\Payload\Address::p()
  * @used-by \Dfe\Stripe\Block\Multishipping::pPhone()
  * @param string[]|OA|QA|CA $n
  * @param bool $throw [optional]
