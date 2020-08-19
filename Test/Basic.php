a99b286d862ff2f14e64bd8df4cefbc2fd996643
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-19 06:22:49 +0600

    https://github.com/mage2pro/core/issues/136

--- Test/Basic.php
+++ Test/Basic.php
@@ -1,6 +1,6 @@
 <?php
 namespace Df\Phone\Test;
-// 2017-04-25
+# 2017-04-25
 final class Basic extends TestCase {
 	/** @test 2017-04-24 */
 	function t01() {}
--- Test/TestCase.php
+++ Test/TestCase.php
@@ -1,5 +1,5 @@
 <?php
 namespace Df\Phone\Test;
-// 2017-04-25
+# 2017-04-25
 /** @see \Df\Phone\Test\Basic */
 abstract class TestCase extends \Df\Payment\TestCase {}
